2a16b64e4cdbf925c59cab541bbf42fef7b7aebf
Andrés Hevia <eternal.aj@gmail.com>
2018-08-17 14:49:36 +0200

    Removed the two dots that show messages.

--- src/dialogues/dialogues-content/dialogues-messages.js
+++ src/dialogues/dialogues-content/dialogues-messages.js
@@ -1,6 +1,21 @@
 const messagesManagement = require('../../database/messages').management
 const dialoguesMessages = {
-  replies: async () => await messagesManagement.retrieve(),
+  replies: async () => {
+    const formattedReplies = {}
+    const arrayFromDB = await messagesManagement.retrieve()
+
+    // -- Initialize the formattedReplies object
+    for (const element of arrayFromDB) {
+      formattedReplies[element.dialogue_name] = []
+    }
+
+    // -- Fill the formattedReplies objects with an array of objects
+    for (const msgs of arrayFromDB) {
+      formattedReplies[msgs.dialogue_name].splice(parseInt(msgs.position_in_dialogue, 10), 0, msgs.message)
+    }
+
+    return formattedReplies
+  },
 
   get messages () {
     return this.replies
