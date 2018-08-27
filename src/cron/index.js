86404c472f3226f97ee5a203a50f87e357986b76
Andrés Hevia <eternal.aj@gmail.com>
2018-08-27 18:55:17 +0200

    Monday quiz prepared.

--- src/cron/index.js
+++ src/cron/index.js
@@ -119,9 +119,9 @@ module.exports.oneTimeFix = async () => {
 module.exports.theWinnerIs = async () => {
   let timeOfWinner
   if (process.env.NODE_ENV === 'develop' || process.env.NODE_ENV === 'quality') {
-    timeOfWinner = '20 14 * * 5'
+    timeOfWinner = '5 00 * * 5'
   } else {
-    timeOfWinner = '30 14 * * 5'
+    timeOfWinner = '5 15 * * 5'
   }
   scheduler.scheduleJob(timeOfWinner, async () => {
     // -- Send the broadcast dialog with the messages of the Quiz
